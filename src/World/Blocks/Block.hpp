fd7086ec268396d2d9725a6e45c9cff04338f29d
Gray7915 <patrick.james.gray15@gmail.com>
2026-07-14 19:41:29 +1200

    new block class to be workede on

---
+++ src/World/Blocks/Block.hpp
@@ -0,0 +1,24 @@
+#include <memory>
+#include "../Rendering/Core/lve_model.hpp"
+
+namespace lve
+{
+    class Block
+    {
+    public:
+        Block(uint16_t id, std::string name) : id{id}, name{name} {}
+        Block(uint16_t id, std::string name)
+
+        uint16_t id;
+
+        std::string name;
+
+        // AABB collision boxes for each rotation
+
+        // Render options struct? contains things like is transparent, ect
+
+        // mesh / model -> optional only for non blocks (things like chest count as non-block)
+
+        int hardness;
+    };
+}

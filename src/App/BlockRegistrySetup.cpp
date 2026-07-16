03f0c31c91c95a8ca2198cb816424eb1fb515761
Gray7915 <patrick.james.gray15@gmail.com>
2026-07-16 16:06:42 +1200

    changed block registry to use otpional

--- src/App/BlockRegistrySetup.cpp
+++ src/App/BlockRegistrySetup.cpp
@@ -7,10 +7,10 @@ namespace lve
     void BlockRegistrySetup::SetupBlockRegistry(BlockRegistry &blockRegistry)
     {
         blockRegistry.Register({.id = uint16_t(0), .name = "Air"});
-        blockRegistry.Register({.id = uint16_t(0), .name = "Grass"});
-        blockRegistry.Register({.id = uint16_t(0), .name = "Dirt"});
-        blockRegistry.Register({.id = uint16_t(0), .name = "Stone"});
+        blockRegistry.Register({.id = uint16_t(1), .name = "Grass"});
+        blockRegistry.Register({.id = uint16_t(2), .name = "Dirt"});
+        blockRegistry.Register({.id = uint16_t(3), .name = "Stone"});
 
-        blockRegistry.Register({.id = uint16_t(0), .name = "Vase", .boundingBoxes = {BoxVolume{.boxSize = glm::vec3(0.25, 1, 0.25)}}, .hardness = 0, .renderType = RenderType::Mesh});
+        blockRegistry.Register({.id = uint16_t(4), .name = "Vase", .boundingBoxes = {BoxVolume{.boxSize = glm::vec3(0.25, 1, 0.25)}}, .highlightBoxSize = {0.5, 1, 0.5}, .hardness = 0, .renderType = RenderType::Mesh});
     }
 }
